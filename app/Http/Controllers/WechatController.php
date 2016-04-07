ef42bc4076cb544f7fbbe0b60e0ff7779642ebe4
fook <xiashuo.he@foxmail.com>
2016-04-07 10:50:43 +0800

    测试

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use EasyWeChat\Foundation\Application;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Log;
 
 class WechatController extends Controller
 {
     //微信入口
-    public function index(Application $wechat){
+    public function index(Application $wechat,Request $request){
+        Log::info(json_encode($request));
         $wechat->server->serve()->send();
     }
 }
