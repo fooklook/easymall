8e18ff0d893f33e64a728659493a582e9045c3fe
fook <xiashuo.he@foxmail.com>
2016-04-07 11:05:49 +0800

    回复消息测试

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -11,7 +11,11 @@ class WechatController extends Controller
 {
     //微信入口
     public function index(Application $wechat,Request $request){
-        Log::info(json_encode($request));
-        $wechat->server->serve()->send();
+        //Log::info(json_encode($request));
+        $server = $wechat->server;
+        $server->setMessageHandler(function($message){
+            return '欢迎使用fooklook回复助手，功能等待完善中。';
+        });
+        $server->serve()->send();
     }
 }
