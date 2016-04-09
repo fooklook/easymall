04f3069f8f4cf614c3bc574b63d19eb3bdb7dbee
fooklook <xiashuo.he@foxmail.com>
2016-04-09 22:33:01 +0800

    分享修改3

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -43,76 +43,77 @@
 </div>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
-    wx.config(<?php echo $jssdk->config(array('onMenuShareQQ', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQZone'), true, true) ?>);
+    wx.config(<?php $jssdk->config(array('onMenuShareQQ', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQZone'), true, true) ?>);
     var shareTitle = '微信分享测试';
     var descContent = '微信分享内容测试';
     var imgUrl = "http://7xo7bi.com1.z0.glb.clouddn.com/note-easybuild.jpg";
     var lineLink = window.location.href;
+    wx.ready(function(){
+        wx.checkJsApi({
+            jsApiList: [
+                'getLocation',
+                'onMenuShareQQ',
+                'onMenuShareQZone',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ],
+            success: function (res) {
+                alert(JSON.stringify(res));
+            }
+        });
+        //分享到朋友圈
+        wx.onMenuShareTimeline({
+            title: shareTitle,
+            link: lineLink,
+            imgUrl: imgUrl,
+            success: function () {
 
-    wx.ready(function(){});
+            },
+            cancel: function () {
 
-    wx.checkJsApi({
-        jsApiList: [
-            'getLocation',
-            'onMenuShareQQ',
-            'onMenuShareQZone',
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage'
-        ],
-        success: function (res) {
-            alert(JSON.stringify(res));
-        }
+            }
+        });
+
+        //分享给好友
+        wx.onMenuShareAppMessage({
+            title: shareTitle,
+            desc: descContent,
+            link: lineLink,
+            imgUrl: imgUrl,
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+
+            },
+            cancel: function () {
+
+            }
+        });
+        //分享到QQ
+        wx.onMenuShareQQ({
+            title: shareTitle,
+            desc: descContent,
+            link: lineLink,
+            imgUrl: imgUrl,
+            success: function () {
+            },
+            cancel: function () {
+            }
+        });
+        //分享到qq空间
+        wx.onMenuShareQZone({
+            title: shareTitle,
+            desc: descContent,
+            link: lineLink,
+            imgUrl: imgUrl,
+            success: function () {
+            },
+            cancel: function () {
+            }
+        });
     });
-    //分享到朋友圈
-    wx.onMenuShareTimeline({
-        title: shareTitle,
-        link: lineLink,
-        imgUrl: imgUrl,
-        success: function () {
 
-        },
-        cancel: function () {
 
-        }
-    });
-
-    //分享给好友
-    wx.onMenuShareAppMessage({
-        title: shareTitle,
-        desc: descContent,
-        link: lineLink,
-        imgUrl: imgUrl,
-        type: '', // 分享类型,music、video或link，不填默认为link
-        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-        success: function () {
-
-        },
-        cancel: function () {
-
-        }
-    });
-    //分享到QQ
-    wx.onMenuShareQQ({
-        title: shareTitle,
-        desc: descContent,
-        link: lineLink,
-        imgUrl: imgUrl,
-        success: function () {
-        },
-        cancel: function () {
-        }
-    });
-    //分享到qq空间
-    wx.onMenuShareQZone({
-        title: shareTitle,
-        desc: descContent,
-        link: lineLink,
-        imgUrl: imgUrl,
-        success: function () {
-        },
-        cancel: function () {
-        }
-    });
 </script>
 </body>
 </html>
