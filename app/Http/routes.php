a3fa70da4a201208502a9e811bc75893dd48e1c8
fooklook <xiashuo.he@foxmail.com>
2016-04-14 21:29:19 +0800

    微博登录5

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,7 +30,7 @@ Route::group(['middleware' => ['web']], function () {
     // 引导用户到新浪微博的登录授权页面
     Route::get('auth/weibo', 'Auth\AuthController@weibo');
     // 用户授权后新浪微博回调的页面
-    Route::get('auth/callback', 'Auth\AuthController@callback')
+    Route::get('auth/callback', 'Auth\AuthController@callback');
 });
 
 Route::any('wechat', 'WechatController@index');
